5e7954689c8ea4e3be3c1bd15d5e316ee3664d71
Jack Shieh <71810019+sme322-ui@users.noreply.github.com>
2022-02-01 22:10:18 +0800

    Add files via upload

---
+++ WeatherInfoKit2/WeatherInfoKit2.h
@@ -0,0 +1,18 @@
+//
+//  WeatherInfoKit2.h
+//  WeatherInfoKit2
+//
+//  Created by Sie monyan on 2022/1/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherInfoKit2.
+FOUNDATION_EXPORT double WeatherInfoKit2VersionNumber;
+
+//! Project version string for WeatherInfoKit2.
+FOUNDATION_EXPORT const unsigned char WeatherInfoKit2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherInfoKit2/PublicHeader.h>
+
+
